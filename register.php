d61b82f2d707550b91645a35eaf467780b9242c7
Appoline <appoline.deangelis@gmail.com>
2020-03-30 15:20:59 +0200

    Créer son compte

--- register.php
+++ register.php
@@ -20,10 +20,18 @@ if(isset($_SESSION["pseudo"])){
 <!-- Formulaire de connexion de session -->
 <div class="form_login">
     <h2>Connexion</h2>
-    <form action="" method="post" class="login">
+    <form action="functions/createUser.php" method="post" class="login">
         <input type="text" placeholder="Pseudo" name="pseudo">
-        <input type="text" placeholder="Rôle" name="role">
         <input type="password" placeholder="Password" name="password">
-        <input type="submit" value="Se connecter">
+        <input type="password" placeholder="Confirm Password" name="confirm_password">
+        
+        <div class="check_box">
+            <p>Souhaites-tu être un Coco Helper ?</p>
+            <label for="role">Oui</label>
+            <input type="checkbox" name="role" id="role" class="form-check-input">
+        </div>
+        
+        
+        <input type="submit" value="Créer mon compte">
     </form>
 </div>
